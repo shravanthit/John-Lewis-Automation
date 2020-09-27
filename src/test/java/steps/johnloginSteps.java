a62b5b53730eb1095824ccf791cb0c0fd702ddf3
Shravanthi.Thuma <sraavanthi.thuma@gmail.com>
2020-09-27 19:36:18 +0100

    new test cases added- day2

--- src/test/java/steps/johnloginSteps.java
+++ src/test/java/steps/johnloginSteps.java
@@ -13,15 +13,17 @@ import utils.DriverFactory;
 public class johnloginSteps {
         WebDriver driver = DriverFactory.driver;
         johnLoginPage johnlewis = PageFactory.initElements( driver, johnLoginPage.class );
-
+        public String e;
+        public String p;
 
         @Given("^I am on Johnlewis Home page$")
         public void on_Linkedin_page() {
-            johnlewis.launchLinkedinPage();
+            johnlewis.launcJohnLewisHomePage();
         }
 
         @When("^I Enter (.*) andalso (.*)$")
         public void i_Enter_details(String email, String pwd) {
+
             johnlewis.enterDetails( email, pwd );
         }
 
@@ -31,5 +33,47 @@ public class johnloginSteps {
             Assert.assertTrue( "Registration not successful", johnlewis.verifyRegistered() );
 
         }
+    @Given("I am loggedin with (.*) and (.*)")
+    public void i_am_loggedin_with_Email_and_Password(String email, String pwd) throws InterruptedException {
+
+        johnlewis.launcJohnLewisHomePage();
+        johnlewis.enterDetails( email , pwd );
+        johnlewis.verifyRegistered();
+
+
+    }
+
+    @When("I logout from the application")
+    public void i_logout_from_the_application() throws Throwable {
+            johnlewis.signOutFromApplication();
+
+    }
+
+    @Then("I should be logged out succesfully")
+    public void i_should_be_logged_out_succesfully() throws Throwable {
+            Assert.assertTrue( "Signout not successfull" ,johnlewis.verifyLogoutSuccessfull() );
+
+    }
+
+    @When("I check my details in account tab")
+    public void checkDetails() throws InterruptedException {
+            johnlewis.checkMyDetails();
+
+    }
+
+    @Then("I should be able to see my details")
+    public void detailsDisplayedSuccesfully(){
+            Assert.assertTrue( "details not displayed", johnlewis.verifyDetailsDisplayed());
+
+    }
+   @When("I click on order tab in the sidebar")
+    public void accountdetailsFromSidebar() throws Throwable{
+       johnlewis.checkAccountOverview();
+    }
+   @Then( "I should be able to see my order details" )
+    public void orderDetails(){
+            Assert.assertTrue( "Order details not displayed", johnlewis.verifyOrders());
+
+    }
     }
 
--- src/test/resources/Johnlogin.feature
+++ src/test/resources/Johnlogin.feature
@@ -10,11 +10,36 @@ Feature: Johnlewis Home Page
       | sraavanthi.thuma@gmail.com | Johnlewis@123  |
 
 @LewisLogout
-Scenario: Logout from the johnlewis application
+Scenario Outline: Logout from the johnlewis application
 
-  Given I am on homePage
-  When  I
+  Given I am loggedin with <Email> and <Password>
+  When  I logout from the application
+  Then  I should be logged out succesfully
+  Examples:
+    | Email               | Password |
+    | sraavanthi.thuma@gmail.com | Johnlewis@123  |
 
+  @LewischeckMydetails
+
+  Scenario Outline: Check mydetails in account tab
+
+    Given I am loggedin with <Email> and <Password>
+    When  I check my details in account tab
+    Then  I should be able to see my details
+    Examples:
+      | Email               | Password |
+      | sraavanthi.thuma@gmail.com | Johnlewis@123  |
+
+  @shadowDomImplementation
+
+  Scenario Outline: check accountoverview from sidebar
+
+    Given I am loggedin with <Email> and <Password>
+    When  I click on order tab in the sidebar
+    Then  I should be able to see my order details
+    Examples:
+      | Email               | Password |
+      | sraavanthi.thuma@gmail.com | Johnlewis@123  |
 
 
 
